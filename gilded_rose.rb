91c0efbfe5d50c63bb11cbc0767282ca324fc7ce
James Gardiner <jamesgardiner075@gmail.com>
2016-12-01 16:57:23 +0000

    Refactor update_quality method

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -7,13 +7,11 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Sulfuras, Hand of Ragnaros"
+      if should_be_updated?(item)
         case item.name
         when "Aged Brie" then update_aged_brie(item)
-        when "Backstage passes to a TAFKAL80ETC concert"
-          update_backstage_passes(item)
-        else
-          update_normal_items(item)
+        when "Backstage passes to a TAFKAL80ETC concert" then update_passes(item)
+        else update_normal_items(item)
         end
         item.sell_in -= 1 if item.name
       end
@@ -22,11 +20,15 @@ class GildedRose
 
   private
 
-  def update_aged_brie(item)
-    item.quality += 1 if item.quality < 50
+  def should_be_updated?(item)
+    item.name != "Sulfuras, Hand of Ragnaros" && item.quality < 50
   end
 
-  def update_backstage_passes(item)
+  def update_aged_brie(item)
+    item.quality += 1
+  end
+
+  def update_passes(item)
     if item.sell_in > 10
       item.quality += 1
     else
@@ -39,7 +41,6 @@ class GildedRose
     item.sell_in <= 0 ? item.quality -= 2 : item.quality -= 1
     item.quality = 0 if item.quality < 0
   end
-
 end
 
 class Item
